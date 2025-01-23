ad97c3ae6344d5279d6c84d002143b51ce47549d
denys <denis.bielykh38@gmail.com>
2025-01-23 12:46:14 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write your code here
+const tableChildren = document.querySelector('table').children;
+const SECOND_COLUMN = 1;
+
+for (const el of tableChildren) {
+  for (const tr of el.children) {
+    const newElem = tr.children[SECOND_COLUMN].cloneNode(true);
+
+    tr.lastElementChild.insertAdjacentElement('beforebegin', newElem);
+  }
+}
