d2b29c9b1d24a48835fe855edc21f32de6fa476c
denys <denis.bielykh38@gmail.com>
2025-01-23 13:00:45 +0200

    solution-2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,14 @@
 'use strict';
 
-const tableChildren = document.querySelector('table').children;
+const tableRows = document.querySelectorAll('table tr');
 const SECOND_COLUMN = 1;
+const MIN_COLUMNS = SECOND_COLUMN + 1;
 
-for (const el of tableChildren) {
-  for (const tr of el.children) {
+for (const tr of tableRows) {
+  if (tr.children.length >= MIN_COLUMNS) {
     const newElem = tr.children[SECOND_COLUMN].cloneNode(true);
 
     tr.lastElementChild.insertAdjacentElement('beforebegin', newElem);
   }
 }
+
